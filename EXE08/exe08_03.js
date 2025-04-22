589eeeff1151949329ab059deac2b38b4382046c
RyanGotinho <ryan.p.freitas6@aluno.senai.br>
2025-04-22 10:21:15 -0300

    EXE08

---
+++ EXE08/exe08_03.js
@@ -0,0 +1,9 @@
+
+
+function FormatarData(dia, mes, ano){
+    let data = `${dia}/${mes}/${ano}`
+    return data
+}
+
+let valorRetornado = FormatarData('19','12','2008')
+console.log(valorRetornado);
